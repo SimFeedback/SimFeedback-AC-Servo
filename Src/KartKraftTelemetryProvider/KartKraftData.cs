3799397661636e95764b7ea0ffd6c2c69159bbb0
Michael Rausch <mra@rauschit.de>
2019-08-28 21:20:51 +0200

    KartKraft Telemetry Provider

---
+++ Src/KartKraftTelemetryProvider/KartKraftData.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimFeedback.telemetry.kartkraft
+{
+    internal class KartKraftData
+    {
+        public float Yaw;
+        public float Pitch;
+        public float Roll;
+        public float AccelerationX;
+        public float AccelerationY;
+        public float AccelerationZ;
+        public float Traction;
+        public float RPM;
+        private float _speed;
+
+        public float Heave
+        {
+            get { return (AccelerationZ / 9.81f); }
+        }
+
+        public float Surge
+        {
+            get { return (AccelerationX / 9.81f); }
+        }
+
+        public float Sway
+        {
+            get { return (AccelerationY / 9.81f); }
+        }
+
+        public float Speed
+        {
+            set { _speed = value; }
+            get { return _speed * 3.6f; }
+        }
+
+    }
+}
