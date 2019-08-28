933ec73ba1719c111be18db10fddc63a2f0e2eaa
Michael Rausch <mra@rauschit.de>
2019-08-29 00:02:14 +0200

    Simple Test Extension

---
+++ Src/extension/TestExtension/TestExtension.cs
@@ -0,0 +1,29 @@
+﻿using SimFeedback.extension;
+using System.Windows.Forms;
+
+namespace TestExtension
+{
+    public class TestExtension : AbstractSimFeedbackExtension
+    {
+        public TestExtension()
+        {
+            Name = "Test Extension";
+            Info = "Simple Test Extension";
+            Version = "1.0.0";
+            Author = "saxxon66";
+            HasControl = false;
+        }
+
+        public override void Start()
+        {
+            MessageBox.Show("Test Extension started.");
+        }
+
+        public override void Stop()
+        {
+            MessageBox.Show("Test Extension stopped.");
+        }
+
+        
+    }
+}
